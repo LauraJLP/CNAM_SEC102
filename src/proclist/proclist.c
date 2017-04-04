1ad62ec830c792a40f22a129f179360d42f5366a
Laura Le Padellec <lauralepadellec@hotmail.com>
2017-04-04 16:18:40 +0200

    Added comments and tidied up.

--- src/proclist/proclist.c
+++ src/proclist/proclist.c
@@ -1,3 +1,17 @@
+/**
+* \file proclist.c
+* \author Laura Le Padellec
+* \date 4 April 2017
+* \brief Implementation of CNAM SEC102 Process/Module enumeration
+*
+* Usage: proclist 
+* 
+* This program will list all processes and for each process found, list the path to its
+* executable, the list of modules loaded and the size and load address for the modules.
+* After each process located the user should hit carriage return to continue program execution
+* this is to enable the user to read the output at his ease.
+*/
+
 #include <Windows.h>
 #include <stdio.h>
 #include <tchar.h>
@@ -5,15 +19,17 @@
 
 #include "proclist.h"
 
-// To ensure correct resolution of symbols, add Psapi.lib to TARGETLIBS
-// and compile with -DPSAPI_VERSION=1
+/**
+* \brief print process name, executable, list of loaded modules and address and size for the modules
+*
+* \param _In_ processID id of the process whose data is to be displayed
+*/
 
-void PrintProcessNameAndID(DWORD processID)
+void printProcessInfo(DWORD processID)
 {
 	TCHAR szProcessName[MAX_PATH] = TEXT("<unknown>");
 	TCHAR imageFileName[MAX_PATH];
 	HANDLE hProcess;
-	DWORD cbNeeded;
 	unsigned int i;
 
 	// Get a handle to the process.
@@ -28,11 +44,11 @@ void PrintProcessNameAndID(DWORD processID)
 	{
 		DWORD cbNeeded;
 		HMODULE * hModList;
-		int initialSize = sizeof(HMODULE) * 4096;
+		DWORD initialSize = sizeof(HMODULE) * 4096;
 
 		if ((hModList = (HMODULE *)malloc(initialSize)) == NULL)
 		{
-			_tprintf(TEXT("\tError allocating memory for module list\n"));
+			printf("\tError allocating memory for module list\n");
 			return;
 		}
 		if (EnumProcessModulesEx(hProcess, hModList, initialSize,
@@ -41,10 +57,9 @@ void PrintProcessNameAndID(DWORD processID)
 			/* if we hadn't allocated enough space to hold all the module info
 			    reallocate here
 				*/
-			_tprintf(TEXT("DEBUG cbNeeded: %d sizeof HMODULE* %d\n", cbNeeded, sizeof(HMODULE)));
 			if (cbNeeded > initialSize) {
 				if (realloc(hModList, cbNeeded) == NULL) {
-					_tprintf(TEXT("\tError allocating memory for module list\n"));
+					printf("\tError allocating memory for module list\n");
 					free(hModList);
 					return;
 				}
@@ -57,7 +72,6 @@ void PrintProcessNameAndID(DWORD processID)
 				GetModuleBaseName(hProcess, hModList[i], szProcessName,
 					sizeof(szProcessName) / sizeof(TCHAR));
 
-
 				// Get the full path to the module's file.
 
 				if (GetModuleFileNameEx(hProcess, hModList[i], szModName,
@@ -65,10 +79,10 @@ void PrintProcessNameAndID(DWORD processID)
 				{
 					// Print the module name and handle value.
 
-					_tprintf(TEXT("\t%s (0x%08X)\n"), szModName, hModList[i]);
+					printf("\t%s (0x%08X)\n", szModName, hModList[i]);
 				}
 				if ((modInfo = (MODULEINFO *)malloc(sizeof(MODULEINFO))) == NULL)
-					_tprintf(TEXT("\tCouldn't allocate memory for module info structure!\n"));
+					printf("\tCouldn't allocate memory for module info structure!\n");
 				else {
 					if ((GetModuleInformation(hProcess, hModList[i], modInfo, sizeof(MODULEINFO)))) {
 						printf("\tModule load address: 0x%08X size: %d bytes\n", modInfo->lpBaseOfDll, modInfo->SizeOfImage);
@@ -83,27 +97,28 @@ void PrintProcessNameAndID(DWORD processID)
 
 	// Print the process name and identifier followed by the path to the executable
 
-	_tprintf(TEXT("%s  (PID: %u)\n"), szProcessName, processID);
+	printf("%s  (PID: %u)\n", szProcessName, processID);
 	if (GetProcessImageFileName(hProcess, imageFileName, sizeof(imageFileName)) > 0)
-		_tprintf(TEXT("%s  \n"), imageFileName);
+		printf("%s  \n", imageFileName);
 
 	// Release the handle to the process.
 
 	CloseHandle(hProcess);
 }
 
+/**
+* \brief main entry point for the application - no args required
+*/
 int main(void)
 {
 	// Get the list of process identifiers.
-
-	DWORD aProcesses[1024], cbNeeded, cProcesses;
-	unsigned int i;
+	DWORD				aProcesses[1024], cbNeeded, cProcesses;
+	unsigned int		i;
+	char				ch;
+	DWORD				read;
 
 	if (!EnumProcesses(aProcesses, sizeof(aProcesses), &cbNeeded))
-	{
 		return 1;
-	}
-
 
 	// Calculate how many process identifiers were returned.
 
@@ -111,13 +126,11 @@ int main(void)
 
 	// Print the name and process identifier for each process.
 
-	for (i = 0; i < cProcesses; i++)
-	{
+	for (i = 0; i < cProcesses; i++) {
 		if (aProcesses[i] != 0)
-		{
-			PrintProcessNameAndID(aProcesses[i]);
-		}
+			printProcessInfo(aProcesses[i]);
+		printf("\n---------------Hit enter to continue------------------\n\n");
+		ReadConsole(GetStdHandle(STD_INPUT_HANDLE), &ch, 1, &read, NULL);
 	}
-
 	return 0;
 }
--- src/proclist/proclist.h
+++ src/proclist/proclist.h
@@ -1,3 +1,10 @@
-#pragma once
-
-unsigned int Usage(char* lpszProgramName);
+/**
+* \file proclist.h
+* \author Laura Le Padellec
+* \date 4 April 2017
+* \brief Implementation of CNAM SEC102 Process/Module enumeration
+*
+* Header file for main module.
+*/
+void printProcessInfo(DWORD processID);
+int main(void);
