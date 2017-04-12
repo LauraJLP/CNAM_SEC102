94939f35727be81e93270d2f0a87745e2c197455
Laura Le Padellec <lauralepadellec@hotmail.com>
2017-04-12 23:57:50 +0200

    Minor update to build.bat batch script - If build directory doesn't exist then create it.

--- build.bat
+++ build.bat
@@ -1,6 +1,7 @@
 set msString=%1
 if [%msString%] == [] set msString="Visual Studio 15 2017"
 echo "msString %msString%"
+if not exist "build" mkdir build
 pushd build
 cmake -G %msString% ../src
 cmake --build . --target Sample
